4b3563367ad85cfdadfd851922c50726aaf23486
Miguel Gasca <mgasca@theworkshop.com>
2018-03-25 18:55:02 +0200

    14 - Creating and editing flow

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -19,3 +19,20 @@ exports.getRecipes = async (req, res) => {
     const recipes = await Recipe.find();
     res.render('recipes', { title: 'Recipes', recipes });
 };
+
+exports.editRecipe = async (req, res) => {
+   const recipe = await Recipe.findOne({ _id: req.params.id });
+   // TODO confirm owner of the store
+   res.render('editRecipe', { title: `Edit ${recipe.title}`, recipe })
+};
+
+exports.updateRecipe = async (req, res) => {
+    const recipe = await Recipe.findOneAndUpdate(
+        { _id: req.params.id },
+        req.body,
+        { new: true, runValidators: true }
+    ).exec();
+    // TODO confirm owner of the store
+    req.flash('success', `Successfully updated <strong>${recipe.title}</strong>. <a href="/recipes/${recipe.slug}">View Recipe</a>`);
+    res.redirect(`/recipes/${recipe._id}/edit`);
+};
--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,7 @@ router.get('/', catchErrors(recipeController.getRecipes));
 router.get('/recipes', catchErrors(recipeController.getRecipes));
 router.get('/add', recipeController.addRecipe);
 router.post('/add', catchErrors(recipeController.createRecipe));
+router.post('/add/:id', catchErrors(recipeController.updateRecipe));
+router.get('/recipes/:id/edit', catchErrors(recipeController.editRecipe));
 
 module.exports = router;
