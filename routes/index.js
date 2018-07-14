9d5caac504ca0543787c7f8c44b5b43249ed69eb
Miguel Gasca <mgasca@theworkshop.com>
2018-07-14 12:57:35 +0200

    26 - Edit Account

--- routes/index.js
+++ routes/index.js
@@ -39,4 +39,7 @@ router.post('/register',
 
 router.get('/logout', authController.logout);
 
+router.get('/account', authController.isLoggedIn, userController.account);
+router.post('/account', catchErrors(userController.updateAccount));
+
 module.exports = router;
